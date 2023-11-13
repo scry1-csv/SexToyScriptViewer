8cd31b34a1f2c9d5113161e977af746902c92140
scry1-csv <monoredgoblins60@gmail.com>
2023-11-13 17:53:08 +0900

    左右で違う動作をする部分を検出する処理を追加 スクリプトの内部表現が読み込んだ時点で左右分割していたのを変更して、読み込んだ時点ではCSVの仕様と同じように左右を同一オブジェクトに格納し、プロットに変換する段階で左右のデータを分割するように変更

--- Script/UFOTW.cs
+++ Script/UFOTW.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -15,8 +14,10 @@ namespace SexToyScriptViewer.Script
         public string FileName { get; init; } = "";
         public string TrackerFormatString { get { return "{1}: {HHMMSS} ({ScriptTime})\n{3}: {4}"; } }
 
+
         // Dataに不適正な内容を直接加えることを防ぐため、隠蔽してメソッドで操作を提供する
-        private (List<ScriptLine> left, List<ScriptLine> right) _scriptData = (new(), new());
+        //private (List<SeparatedScriptLine> left, List<SeparatedScriptLine> right) _separatedScriptData = (new(), new());
+        private List<ScriptLine> _scriptData = new();
 
         public static int Validate(string csv_str)
         {
@@ -38,19 +39,45 @@ namespace SexToyScriptViewer.Script
 
         public string LabelFormatter_ScriptTime(double milliseconds) => (milliseconds / 100).ToString();
 
+        public List<(double start, double end)> DetectDeference()
+        {
+            List<(double start, double end)> result = new();
+            bool prevWasDiffernt = false;
+            double tmp_start = 0, tmp_end = 0;
+            foreach (var l in _scriptData)
+            {
+                if (l.LeftDirection != l.RightDirection | l.LeftPower != l.RightPower)
+                {
+                    if (!prevWasDiffernt)
+                    {
+                        prevWasDiffernt = true;
+                        tmp_start = l.Milliseconds;
+                    }
+                }
+                else
+                {
+                    if (prevWasDiffernt)
+                    {
+                        prevWasDiffernt = false;
+                        tmp_end = l.Milliseconds;
+                        result.Add((tmp_start, tmp_end));
+                    }
+                }
+            }
+            return result;
+        }
+
         public static UFOTW? LoadScript(string path)
         {
             using var f = new StreamReader(path);
-            
+
             var csv_str = f.ReadToEnd();
 
 
             var lines = ScriptUtil.RawCsvToLines(csv_str);
 
-            (List<ScriptLine> left, List<ScriptLine> right) result = (new(), new());
+            List<ScriptLine> script = new();
 
-            ScriptLine prevLeft = new();
-            ScriptLine prevRight = new();
             foreach (var line in lines)
             {
                 if (!ValidatorRegex().IsMatch(line))
@@ -58,34 +85,24 @@ namespace SexToyScriptViewer.Script
                 var splitted = line.Split(',');
 
                 int time = int.Parse(splitted[0]);
-                bool direction1 = splitted[1] == "1";
-                bool direction2 = splitted[3] == "1";
+                bool leftDirection = splitted[1] == "1";
+                bool rightDirection = splitted[3] == "1";
+                int leftPower = int.Parse(splitted[2]);
+                int rightPower = int.Parse(splitted[4]);
 
-                ScriptLine left = new()
+                script.Add(new ScriptLine()
                 {
                     InternalTime = time,
-                    Direction = direction1,
-                    Power = int.Parse(splitted[2])
-                };
-
-                ScriptLine right = new()
-                {
-                    InternalTime = time,
-                    Direction = direction2,
-                    Power = int.Parse(splitted[4])
-                };
-
-                if (left != prevLeft)
-                    result.left.Add(left);
-                prevLeft = left;
-                if (right != prevRight)
-                    result.right.Add(right);
-                prevRight = right;
+                    LeftDirection = leftDirection,
+                    LeftPower = leftPower,
+                    RightDirection = rightDirection,
+                    RightPower = rightPower
+                });
             }
 
             return new UFOTW()
             {
-                _scriptData = result,
+                _scriptData = script,
                 FileName = Path.GetFileName(path),
             };
         }
@@ -94,19 +111,24 @@ namespace SexToyScriptViewer.Script
         {
             List<CustomDataPoint> result = new() { new CustomDataPoint(0, 0, 0) };
 
+            bool prevDirection = true;
             int prevPower = 0;
 
-            foreach (var line in _scriptData.left)
+            foreach (var line in _scriptData)
             {
-                int power;
-                if (line.Direction == true)
-                    power = line.Power;
-                else
-                    power = -line.Power;
+                if (line.LeftDirection != prevDirection || line.LeftPower != prevPower)
+                {
+                    int power;
+                    if (line.LeftDirection == true)
+                        power = line.LeftPower;
+                    else
+                        power = -line.LeftPower;
 
-                result.Add(new CustomDataPoint(line.Milliseconds, prevPower, line.InternalTime));
-                result.Add(new CustomDataPoint(line.Milliseconds, power, line.InternalTime));
-                prevPower = power;
+                    result.Add(new CustomDataPoint(line.Milliseconds, prevPower, line.InternalTime));
+                    result.Add(new CustomDataPoint(line.Milliseconds, power, line.InternalTime));
+                    prevDirection = line.LeftDirection;
+                    prevPower = line.LeftPower;
+                }
             }
 
             return result.ToArray();
@@ -117,29 +139,53 @@ namespace SexToyScriptViewer.Script
         {
             List<CustomDataPoint> result = new() { new CustomDataPoint(0, 0, 0) };
 
+            bool prevDirection = true;
             int prevPower = 0;
 
-            foreach (var line in _scriptData.right)
+            foreach (var line in _scriptData)
             {
-                int power;
-                if (line.Direction == true)
-                    power = line.Power;
-                else
-                    power = -line.Power;
+                if (line.RightDirection != prevDirection || line.RightPower != prevPower)
+                {
+                    int power;
+                    if (line.RightDirection == true)
+                        power = line.RightPower;
+                    else
+                        power = -line.RightPower;
 
-                result.Add(new CustomDataPoint(line.Milliseconds, prevPower, line.InternalTime));
-                result.Add(new CustomDataPoint(line.Milliseconds, power, line.InternalTime));
-                prevPower = power;
+                    result.Add(new CustomDataPoint(line.Milliseconds, prevPower, line.InternalTime));
+                    result.Add(new CustomDataPoint(line.Milliseconds, power, line.InternalTime));
+                    prevDirection = line.RightDirection;
+                    prevPower = line.RightPower;
+                }
             }
 
             return result.ToArray();
         }
 
+        public record ScriptLine
+        {
+            public int InternalTime;
+            public bool LeftDirection;
+            public int LeftPower;
+            public bool RightDirection;
+            public int RightPower;
+            public double Milliseconds { get => (double)InternalTime * 100; }
+            public override string ToString()
+            {
+                var builder = new StringBuilder();
+                builder.Append($"InternalTime:{InternalTime}, ");
+                builder.Append($"LeftDirection:{LeftDirection}, ");
+                builder.Append($"LeftPower:{LeftPower}");
+                builder.Append($"RightDirection:{RightDirection}, ");
+                builder.Append($"RightPower:{RightPower}");
+                return builder.ToString();
+            }
+        }
 
         /// <summary>
         /// csvの行のデータを保持する構造体
         /// </summary>
-        public record ScriptLine
+        public record SeparatedScriptLine
         {
             public int InternalTime;
             public bool Direction;
